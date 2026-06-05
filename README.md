7dc133edb7a35adcf135e1482e50572f01ff5ff1
trapano-monogamo <filippo.cozzi.fc@gmail.com>
2026-06-05 19:30:59 +0200

    .

--- README.md
+++ README.md
@@ -62,7 +62,9 @@ just two bits because it will always be one of the four general purpose register
 When an instruction takes on register and one immediate/address then the operand B is
 treated as a single byte (only low) or word (low and high), when an instruction takes two
 registers as arguments, then the low byte of B is interpreted as a register (with padding) and
-the high byte is discarded. The encoding is as follows
+the high byte is discarded.
+
+The first bit of the opcode signals whether the instruction has register-register arguments.
 
 | Register | Bits |
 |-|-|
@@ -77,54 +79,54 @@ Data movement:
 
 | Mnemonic | Opcode | A | B | Cycles | Description | Flags |
 |-|-|-|-|-|-|-|
-| LOADI | ??? | reg | imm16  | ??? | loads B to A | ??? |
-| LOAD  | ??? | reg | addr16 | ??? | loads B to A | ??? |
-| STORE | ??? | reg | addr16 | ??? | store A to addr B | ??? |
-| MOVE  | ??? | reg | reg  | ??? | moves content of A to B | ??? |
+| LOADI | 0b000001 | reg | imm16  | ??? | loads B to A | ??? |
+| LOAD  | 0b000010 | reg | addr16 | ??? | loads B to A | ??? |
+| STORE | 0b000011 | reg | addr16 | ??? | store A to addr B | ??? |
+| MOVE  | 0b100100 | reg | reg  | ??? | moves content of A to B | ??? |
 
 Arithmetic:
 
 | Mnemonic | Opcode | A | B | Cycles | Description | Flags |
 |-|-|-|-|-|-|-|
-| ADDI | ??? | reg | imm8 | ??? | adds B to A | ??? |
-| ADDR | ??? | reg | reg  | ??? | adds B to A | ??? |
-| SUBI | ??? | reg | imm8 | ??? | subtracts B from A | ??? |
-| SUBR | ??? | reg | reg  | ??? | subtracts B from A | ??? |
-| INC  | ??? | reg |      | ??? | increments A | ??? |
-| DEC  | ??? | reg |      | ??? | decrements A | ??? |
+| ADDI | 0b000101 | reg | imm8 | ??? | adds B to A | ??? |
+| ADDR | 0b100110 | reg | reg  | ??? | adds B to A | ??? |
+| SUBI | 0b000111 | reg | imm8 | ??? | subtracts B from A | ??? |
+| SUBR | 0b101000 | reg | reg  | ??? | subtracts B from A | ??? |
+| INC  | 0b001001 | reg |      | ??? | increments A | ??? |
+| DEC  | 0b001010 | reg |      | ??? | decrements A | ??? |
 
 Logic:
 
 | Mnemonic | Opcode | A | B | Cycles | Description | Flags |
 |-|-|-|-|-|-|-|
-| AND | ??? | reg | reg | ??? | A & B | ??? |
-| OR  | ??? | reg | reg | ??? | A \| B | ??? |
-| XOR | ??? | reg | reg | ??? | A ^ B | ??? |
-| NOT | ??? | reg |     | ??? | sets A to ~B | ??? |
+| AND | 0b101011 | reg | reg | ??? | A & B | ??? |
+| OR  | 0b101100 | reg | reg | ??? | A \| B | ??? |
+| XOR | 0b101101 | reg | reg | ??? | A ^ B | ??? |
+| NOT | 0b001110 | reg |     | ??? | sets A to ~B | ??? |
 
 Comparison:
 
 | Mnemonic | Opcode | A | B | Cycles | Description | Flags |
 |-|-|-|-|-|-|-|
-| CMPR | ??? | reg | reg   | ??? | sets flags from A-B | ??? |
-| CMPI | ??? | reg | imm8  | ??? | sets flags from A-B | ??? |
-| CMPA | ??? | reg | add16 | ??? | sets flags from A-B | ??? |
+| CMPR | 0b101111 | reg | reg   | ??? | sets flags from A-B | ??? |
+| CMPI | 0b010000 | reg | imm8  | ??? | sets flags from A-B | ??? |
+| CMPA | 0b010001 | reg | add16 | ??? | sets flags from A-B | ??? |
 
 Control flow:
 
 | Mnemonic | Opcode | A | B | Cycles | Description | Flags |
 |-|-|-|-|-|-|-|
-| JMP  | ??? | reg | addr16 | ??? | sets PC to B | ??? |
-| JZ   | ??? |     | addr16 | ??? | sets PC to B if Z=1 (i.e. if zero flag is true) | ??? |
-| JNZ  | ??? |     | addr16 | ??? | sets PC to B if Z=0 (i.e. if zero flag is false) | ??? |
-| JC   | ??? |     | addr16 | ??? | sets PC to B if C=1 (i.e. if carry flag is true) | ??? |
-| JN   | ??? |     | addr16 | ??? | sets PC to B if N=1 (i.e. if negative flag is true) | ??? |
-| CALL | ??? | ??? | ???    | ??? | ??? | ??? |
-| RET  | ??? | ??? | ???    | ??? | ??? | ??? |
+| JMP  | 0b010010 | reg | addr16 | ??? | sets PC to B | ??? |
+| JZ   | 0b010011 |     | addr16 | ??? | sets PC to B if Z=1 (i.e. if zero flag is true) | ??? |
+| JNZ  | 0b010100 |     | addr16 | ??? | sets PC to B if Z=0 (i.e. if zero flag is false) | ??? |
+| JC   | 0b010101 |     | addr16 | ??? | sets PC to B if C=1 (i.e. if carry flag is true) | ??? |
+| JN   | 0b010110 |     | addr16 | ??? | sets PC to B if N=1 (i.e. if negative flag is true) | ??? |
+| CALL | 0b010111 | ??? | ???    | ??? | ??? | ??? |
+| RET  | 0b011000 | ??? | ???    | ??? | ??? | ??? |
 
 System:
 
 | Mnemonic | Opcode | A | B | Cycles | Description | Flags |
 |-|-|-|-|-|-|-|
-| NOP  | ??? | | | ??? | no operation | ??? |
-| HALT | ??? | | | ??? | halts execution | ??? |
+| NOP  | 0b011001 | | | ??? | no operation | ??? |
+| HALT | 0b011010 | | | ??? | halts execution | ??? |
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,197 @@
+#![allow(non_snake_case, unused_variables, dead_code)]
+
+use std::io; // stdin(), stdout()
+use std::io::Write; // flush()
+use unit_enum::UnitEnum; // UnitEnum, from_ordinal()
+
+type Byte  = u8;
+type Imm8  = u8;
+type Word  = u16;
+type Imm16 = u16;
+type Addr  = u16;
+
+#[derive(Debug, UnitEnum)]
+enum Register {
+    R0, R1, R2, R3
+}
+
+#[derive(Debug)]
+enum Instruction {
+    LOADI(Register,Imm16),
+    LOAD(Register,Addr),
+    NOP,
+}
+
+struct CPU {
+    pc: u16,
+    sp: u16,
+    registers: [u8; 4],
+    flags: u8,
+}
+
+struct RAM {
+    mem: [u8; 65536], // 64KiB
+}
+
+struct Machine {
+    cpu: CPU,
+    ram: RAM,
+    halted: bool,
+}
+
+impl CPU {
+    fn new() -> Self {
+        CPU{
+            pc: 0x0000,
+            sp: 0x0000, // !!! CHECK MEMORY MAP SPECIFICATION !!!
+            registers: [0; 4],
+            flags: 0b00000000
+        }
+    }
+
+    fn reset(&mut self) {
+        self.pc = 0x0000;
+        self.sp = 0x0000; // !!! CHECK MEMORY MAP SPECIFICATION !!!
+        self.registers = [0; 4];
+        self.flags = 0b00000000;
+    }
+}
+
+impl RAM {
+    fn new() -> Self {
+        RAM{ mem: [0; 65536] }
+    }
+
+    fn reset(&mut self)  {
+        self.mem = [0; 65536];
+    }
+
+    fn read(&self, addr:Addr) -> Byte {
+        self.mem[addr as usize]
+    }
+}
+
+impl Machine {
+    fn new() -> Self {
+        Machine{ cpu: CPU::new(), ram: RAM::new(), halted: true }
+    }
+
+    fn start(&mut self) {
+        self.cpu.reset();
+        self.ram.reset();
+        // self.load_program();
+        self.halted = false;
+    }
+
+    fn log_state(&self) -> String {
+        format!(
+"
+=====================================
+PC = 0x{pc:0>6X}, SP = 0x{sp:0>6X}
+Flags = {flags:b}
++----+----+----+----+
+| R0 | R1 | R2 | R3 |
++----+----+----+----+
+| {r0:0>2x} | {r1:0>2x} | {r2:0>2x} | {r3:0>2x} |
++----+----+----+----+
+Cycles executed: ???
+=====================================",
+            pc=self.cpu.pc,
+            sp=self.cpu.sp,
+            r0=self.cpu.registers[0],
+            r1=self.cpu.registers[1],
+            r2=self.cpu.registers[2],
+            r3=self.cpu.registers[3],
+            flags=self.cpu.flags
+        )
+    }
+
+    fn log_memory(&self) -> String {
+        format!("NOPE")
+    }
+
+    fn halt(&mut self) {
+        self.halted = true;
+    }
+
+    fn fetch(&mut self) -> [Byte;3] {
+        self.cpu.pc += 3;
+        let pc = &self.cpu.pc;
+        let ram = &self.ram;
+        return [ram.read(pc-3), ram.read(pc-2), ram.read(pc-1)];
+    }
+
+    fn decode(&self, bytes: [Byte;3]) -> Instruction {
+        let opcode: u8     = bytes[0] >> 2;
+        let argA: Register = Register::from_ordinal((bytes[0] & 0b000011) as usize).unwrap();
+        let argBlo: Byte   = bytes[1];
+        let argBhi: Byte   = bytes[2];
+        // let reg_reg: bool = ((opcode & 0b100000) == 0b100000);
+        match opcode {
+            0b100001 => Instruction::LOADI(argA, (((argBlo as u16) << 8) | argBhi as u16) as Imm16),
+            0b100010 => Instruction::LOAD(argA, (((argBlo as u16) << 8) | argBhi as u16) as Addr),
+            _ => Instruction::NOP,
+        }
+    }
+
+    fn step(&mut self) {
+        let instr_bytes = self.fetch();
+        let instr = self.decode(instr_bytes);
+        println!("{:?}",instr);
+        // !!! EXECUTE INSTRUCTION !!!
+    }
+}
+
+enum RunMode {
+    Continuous,
+    Step,
+    Halted,
+}
+
+fn process_step_input(input: &String, machine: &Machine, mode: &mut RunMode) {
+    match input.as_str().trim() {
+        "s" | "step" => { },
+        "c" | "continue" => {
+            *mode = RunMode::Continuous;
+        },
+        _ => { println!("[ERROR]: couldn't recognize command."); }
+    };
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut mode = RunMode::Step;
+    let mut aster8: Machine = Machine::new();
+    aster8.start();
+
+    aster8.ram.mem[0] = 0b10000100;
+    aster8.ram.mem[1] = 0b11111111;
+    aster8.ram.mem[2] = 0b01010101;
+
+    let mut buffer = String::new();
+    let stdin = io::stdin();
+    let mut stdout = io::stdout();
+
+    // let bytes: [u8;3] = [0b11111100, 0b0, 0b0];
+    // println!("{:b}", bytes[0] << 2);
+    // return;
+
+    while !aster8.halted {
+        match mode {
+            RunMode::Continuous => {
+                if aster8.halted == true { mode = RunMode::Halted; }
+                aster8.step();
+            },
+            RunMode::Step => {
+                buffer.clear();
+                print!("{}\ndbg> ", aster8.log_state());
+                stdout.flush().unwrap();
+                stdin.read_line(&mut buffer).unwrap(); // !!! HANDLE RESULT !!!
+                process_step_input(&buffer, &aster8, &mut mode);
+                aster8.step();
+            },
+            RunMode::Halted => {
+                aster8.halt();
+            },
+        };
+    }
 }
